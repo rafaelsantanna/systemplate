ecd0dbd77d2c1de2d6f171b6000ac74ee9ad166c
Rafael Santanna <rafaeel.16@gmail.com>
2018-07-19 23:16:48 -0300

    add conditional

--- admin.php
+++ admin.php
@@ -34,10 +34,10 @@
         <div class="row">
             <div class="col-md-8">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-5">
                     <h1>Upload banner</h1>
                 </div>
-                <div class="col-md-2 d-flex align-items-center">
+                <div v-if="previewImage != ''" class="col-md-2 d-flex align-items-center">
                     <a class="mr-2" href="/systemplate/admin.php">
                         <img src="./icons/home-solid.svg" alt="icon copy" width="30" height="30">
                     </a>
