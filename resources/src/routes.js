89040acfdaa2722507f2493cdc19e6f132714d79
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-09-29 22:02:57 -0300

    add route / to login

--- resources/src/routes.js
+++ resources/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Login from './pages/Login';
 import Template from './pages/Template';
 import TemplateList from './pages/TemplateList';
 
 export default function Routes() {
     return (
         <BrowserRouter>
+            <Route exact path="/" component={Login} />
             <Route path="/template" component={Template} />
             <Route path="/templatelist" component={TemplateList} />
         </BrowserRouter>
