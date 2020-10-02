143cd21229281b67b00ec16b84710eb0ec086c40
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-10-02 17:18:58 -0300

    Create Page Components Signup and Admin

---
+++ resources/src/pages/Admin/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Admin() {
+    return (
+        <div>Create, Edit and Delete Users</div>
+    );
+}
--- resources/src/pages/Users/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Users() {
-    return (
-        <div>Create/Edit Users</div>
-    );
-}
