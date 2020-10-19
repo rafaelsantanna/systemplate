e8c538ef9b7b7d5d729e72ad7434d2f4ce9a5e5a
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-10-19 14:54:52 -0300

    Now the route is accepting params from the react router dom

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
-Route::view('/{path?}', 'app'); // Redirect to React APP
+Route::get('/{path?}', function () {
+  return view('app');
+})->where('path', '.*');
