a244388f63a9c3166be0acfd86c25c8142021b52
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-11-03 20:04:46 -0300

    Setting Cors

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-
 Route::group([
     'prefix' => 'auth'
 ], function () {
