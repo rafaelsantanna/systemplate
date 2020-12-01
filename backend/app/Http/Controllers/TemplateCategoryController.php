63ff636da4efc83118f12369ceb60755beb2cbdb
Rafael Sant Anna <rafaeel.16@gmail.com>
2020-12-01 20:05:50 -0300

    Create controller and Model for TemplateCategory

---
+++ backend/app/Http/Controllers/TemplateCategoryController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TemplateCategory;
+use Illuminate\Http\Request;
+
+class TemplateCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = TemplateCategory::all();
+
+        return response()->json($categories);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TemplateCategory  $templateCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TemplateCategory $templateCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TemplateCategory  $templateCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TemplateCategory $templateCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TemplateCategory  $templateCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TemplateCategory $templateCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TemplateCategory  $templateCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TemplateCategory $templateCategory)
+    {
+        //
+    }
+}
